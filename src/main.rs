dd4aff71b92e557c4729d7e634cbab1a918c351e
hector3211 <hectororopesa5@gmail.com>
2023-02-16 21:34:11 -0500

    [added]: completed create_usr route

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use axum::{
     Json, Router, extract::{Path, State},
 };
 use models::{User, NewUser};
-use schema::users;
 use serde::{Deserialize, Serialize};
 
 use std::net::SocketAddr;
@@ -36,8 +35,9 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user))
-        .route("/users/:user_id/:user_name", get(user_info))
+        // .route("/users", post(create_user))
+        // .route("/users/:user_id/:user_name", get(user_info))
+        .route("/users/:user_name/:user_pw", post(create_user_db))
         .with_state(pool);
 
         
@@ -55,6 +55,7 @@ async fn create_user_db(
     Path((user_name,user_pw)): Path<(String,String)>,
     State(pool): State<DbPool>
 ) -> Result<StatusCode,StatusCode> {
+    let mut conn = pool.get().unwrap();
     use crate::schema::users::dsl::*;
     let new_user = NewUser {
         username: &user_name,
@@ -62,64 +63,62 @@ async fn create_user_db(
     };
     diesel::insert_into(users)
         .values(&new_user)
-        .execute(&mut pool)
+        .execute(&mut conn)
         .expect("Error creating user");
 
     Ok(StatusCode::OK)
-
-
-
-}
-
-#[derive(Deserialize,Serialize)]
-struct SayName {
-    id: i32,
-    name: String,
-}
-
-async fn user_info(
-    Path((user_id,user_name)): Path<(i32,String)>
-) -> Result<Json<SayName>, StatusCode> {
-    let user = SayName {
-        id: user_id,
-        name: user_name,
-    };
-
-    Ok(Json(user))
 }
 
 
+// #[derive(Deserialize,Serialize)]
+// struct SayName {
+//     id: i32,
+//     name: String,
+// }
+
+// async fn user_info(
+//     Path((user_id,user_name)): Path<(i32,String)>
+// ) -> Result<Json<SayName>, StatusCode> {
+//     let user = SayName {
+//         id: user_id,
+//         name: user_name,
+//     };
+//
+//     Ok(Json(user))
+// }
+//
+//
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
+// async fn create_user(
+//     // this argument tells axum to parse the request body
+//     // as JSON into a `CreateUser` type
+//     Json(payload): Json<CreateUser>,
+// ) -> (StatusCode, Json<User>) {
+//     // insert your application logic here
+//     let user = User {
+//         id: 1337,
+//         username: payload.username,
+//     };
+//
+//     // this will be converted into a JSON response
+//     // with a status code of `201 Created`
+//     (StatusCode::CREATED, Json(user))
+// }
+//
+//
+// // the input to our `create_user` handler
+// #[derive(Deserialize)]
+// struct CreateUser {
+//     username: String,
+// }
+//
+// // the output to our `create_user` handler
+// #[derive(Serialize)]
+// struct User {
+//     id: u64,
+//     username: String,
+// }
