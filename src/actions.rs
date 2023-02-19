48c9ce89dcf7208af6c506ab7e47be3d78b557ff
hector3211 <hectororopesa5@gmail.com>
2023-02-18 23:50:57 -0500

    [added]: made update,delete fn for users and added some logs for async fn's

--- src/actions.rs
+++ src/actions.rs
@@ -1,5 +1,5 @@
-use diesel::prelude::*;
-use crate::models::{User,NewUser,Todo,NewTodo};
+use diesel::{prelude::*, helper_types::FindBy};
+use crate::{models::{User,NewUser,Todo,NewTodo}, schema::users};
 use diesel::PgConnection;
 
 pub type DbError = Box<dyn std::error::Error + Send + Sync>;
@@ -8,7 +8,7 @@ pub type DbError = Box<dyn std::error::Error + Send + Sync>;
 pub fn get_users(conn: &mut PgConnection) -> Result<Vec<User>,DbError> {
     use crate::schema::users::dsl::*;
     let data:Vec<User> = users
-        .load(conn)
+        .get_results(conn)
         .expect("Error getting users");
     Ok(data)
 }
@@ -17,17 +17,60 @@ pub fn create_user(
     conn: &mut PgConnection,
     name:String,
     pw:String
-) -> Result<(), DbError> {
+) -> Result<User, DbError> {
     use crate::schema::users::dsl::*;
     let new_user = NewUser {
         username: &name,
         hashed_password: &pw,
     };
 
-    diesel::insert_into(users)
+    let result = diesel::insert_into(users)
         .values(&new_user)
-        .execute(conn)
+        .get_result(conn)
         .expect("Error creating ueser");
 
+    Ok(result)
+}
+
+// Delete user
+pub fn delete_user(
+    user_id: i32,
+    conn: &mut PgConnection
+) -> Result<(), DbError> {
+    use crate::schema::users::dsl::*;
+    diesel::delete(users.filter(id.eq(user_id)))
+        .execute(conn)
+        .expect("Error deleting user");
     Ok(())
 }
+
+// Update user
+pub fn update_user_info(
+    user_name: String,
+    user_pw: String,
+    user_id: i32,
+    conn: &mut PgConnection
+) -> Result<(), DbError> {
+    use crate::schema::users::dsl::*;
+    diesel::update(users.filter(id.eq(user_id)))
+        .set((
+            username.eq(user_name),
+            hashed_password.eq(user_pw)
+        ))
+        .execute(conn)
+        .expect("Error updating user");
+
+        Ok(())
+}
+//
+//
+//
+
+// Get Todos
+//
+//
+// Create Todo
+//
+// Delete Todo
+//
+// Update Todo
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use axum::{
     http::StatusCode, Router, extract::{Path, State}, response::IntoResponse, Json,
 };
 use models::{User, Res};
+use tracing::{debug_span, info_span, info, warn, instrument};
+use tracing_subscriber::field::debug;
 use std::net::SocketAddr;
 use diesel::{
     r2d2::{self, ConnectionManager},
@@ -14,22 +16,28 @@ use diesel::{
 };
 use dotenvy::dotenv;
 use std::env;
+use tracing_futures::Instrument;
 
 
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[tokio::main]
 async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
     dotenv().ok();
     let database_url = env::var("DATABASE_URL")
         .expect("No DATABASE_URL provided!");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool = r2d2::Pool::builder()
+        .max_size(5)
         .build(manager)
         .expect("Failed to create pool!");
 
 
     let app = Router::new()
+        .route("/test", get(async_test))
         .route("/", get(get_user))
         .route("/user/:user_name/:user_pw", post(create_user))
         .with_state(pool.clone());
@@ -38,22 +46,34 @@ async fn main() {
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
+async fn async_test() -> &'static str {
+    info!("handling request");
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    warn!("response delayed");
+
+    "hello world"
+}
+
+#[instrument]
 async fn get_user(
     State(state): State<DbPool>
 ) -> Result<Json<Vec<User>>,StatusCode> {
 
+    info!("Started Tokio async");
     let users = tokio::task::spawn_blocking(move ||{
         let mut conn = state.get()?;
         actions::get_users(&mut conn)
     })
     .await
     .unwrap();
+    warn!("Ended Tokio async");
 
     if let Ok(users) = users {
         Ok(Json(users))
@@ -64,31 +84,20 @@ async fn get_user(
 }
 
 
+#[instrument]
 async fn create_user(
     Path((user_name,user_pw)): Path<(String,String)>,
     State(state): State<DbPool>
-) -> Result<impl IntoResponse,StatusCode> {
+) -> Result<(StatusCode,Json<User>),StatusCode> {
 
-    let insert = tokio::task::spawn_blocking(move || {
+    let new_user = tokio::task::spawn_blocking(move || {
         let mut conn = state.get()?;
         actions::create_user(&mut conn, user_name, user_pw)
     })
     .await
     .unwrap();
 
-    if let Ok(insert) = insert {
-        let message = Res {
-            message: "Successfully made a new user!".to_string(),
-            status: 200,
-        };
-        Ok((StatusCode::OK,Json(message)))
-    } else {
-        let message = Res {
-            message: "Error creating new user".to_string(),
-            status: 404,
-        };
-        Ok((StatusCode::BAD_REQUEST,Json(message)))
-    }
+    Ok((StatusCode::OK,Json(new_user.ok().unwrap())))
 }
 
 
